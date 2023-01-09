c2ebed5792ec8c77337788fe6ab5088ea0ea1ffe
spalivel <spalivel@usc.edu>
2023-01-10 03:35:01 +0530

    mail

--- script.js
+++ script.js
@@ -49,6 +49,42 @@ function checkScroll() {
 
 
 
+document.getElementById("contact-form").addEventListener("submit",(event)=> {
+    event.preventDefault();
+    let name= document.getElementById("input-name").value;
+    let email= document.getElementById("input-email").value;
+    let message= document.getElementById("input-message").value;
+    location.href = "mailto:"+"saiprasadpalivela@gmail.com"+'?cc='+'&subject='+email+'&body='+message;
+
+//     if(name==" " || email==" " || message==" "){
+//         return;
+//     }
+//     else{
+//     console.log(name,email,message);
+//   Email.send({
+//     Host :" smtp.elasticemail.com",
+//     SecureToken:"4E175877606C2D63BD084A8C82D0A5C6FFBB22D65E8D19EF60BB2EBF6A40DD63E9701DB21F7BF88AE53C8F9352F4E141",
+//     Username: "saiprasadpalivela@gmail.com",
+//     UseDefaultCredentials:false,
+//     Password: "joel123$*",
+//     EnableSsl:true,
+//     To: 'saiprasadpalivela@gmail.com',
+//     From: "saiprasadpalivela@gmail.com",
+//     Subject: "Sending Email using javascript",
+//     Body: `Name: ${name} email: ${email} message: ${message}`,
+//   })
+//     .then(function (message) {
+//         console.log(message);
+//       alert("message sent successfully")
+//     });
+// }
+});
+
+// document.getElementById("contact-form").onsubmit(function(event){
+// event.preventDefault();
+// sendEmail();
+// });
+
 window.addEventListener("scroll", checkScroll);
 
 // This event fills the progress bars if they are displayed on the screen when the page is loaded.
